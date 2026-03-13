363330e1d00b14f49e84e688ec3d88add7c79420
MohammedZ666 <abuobaidazishan@gmail.com>
2026-03-14 02:02:06 +0600

    saved embeddings to supabase

---
+++ utils/embedding_engine.py
@@ -0,0 +1,39 @@
+import os
+from typing import List
+from google import genai
+from google.genai import types
+from langchain_core.embeddings import Embeddings
+
+
+
+class GeminiEmbedder(Embeddings):
+    def __init__(self, api_key: str, model: str = "gemini-embedding-2-preview"):
+        self.client = genai.Client(api_key=api_key)
+        self.model = model
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        embeddings = self.client.models.embed_content(
+        model=self.model,
+        contents=texts,
+        config=types.EmbedContentConfig(
+            task_type="retrieval_document",
+         )
+        ).embeddings
+
+        return [embedding.values for embedding in embeddings]
+
+    def embed_query(self, text: str) -> List[float]:
+        embeddings = self.client.models.embed_content(
+        model=self.model,
+        contents=text,
+        config=types.EmbedContentConfig(
+            task_type="retrieval_document",
+         )
+       ).embeddings
+        return embeddings[0].values
+
+
+embedding_engine = GeminiEmbedder(
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    model="gemini-embedding-2-preview",
+)
